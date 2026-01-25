e1f05b354ae0d69637a4275cf3e17c7eb7812f14
Jose Alves <josevitoralves561@gmail.com>
2026-01-25 15:17:00 +0100

    doing some stuff on monte carlo code

--- tp2.py
+++ tp2.py
@@ -1,36 +1,44 @@
 import  numpy as np
 from matplotlib import pyplot as plt
+import tp1 
 
 
 class Exercice():
 
-    def __init__(self , a , b , n):
-        self.a = a
-        self.b = b
-        self.n = n
-        random_array = np.random.uniform(low = a, high=b , size = n)
+    def __init__(self , low=0 , high=1 ):
+        self.low = low
+        self.high = high
+        self.densite_probabilite = None
         
-
-    @staticmethod 
-    def g(x):
+ 
+    def g(self , x):
         return 2 * (np.exp(x) - 1)/(np.e - 1)
         
 
     def h(self , x):
-        return self.g(x) / self.densite_uniforme()
-
-    @staticmethod
-    def G(x):
+        return self.g(x) / self.densite_probabilite()
+    
+    def integrand(self , x):
+        return self.h(x)**2 * self.densite_probabilite()
+    
+    def G(self , x):
         return 2/(np.e - 1) * (np.exp(x) - x)
 
     def densite_uniforme(self):
-        return 1/(self.b-self.a)
+        return 1/(self.high-self.low)
+    
+    def set_densite_probabilite(self , p) :
+        self.densite_probabilite = p
 
 
 if __name__ == "__main__":
-    e = Exercice()
-    exact_value = e.G(1) - e.G(0)
-    print(exact_value)
+    e = Exercice(low = 0 , high= 1 )
+    e.set_densite_probabilite(e.densite_uniforme)
+    exact_value = e.G(e.high) - e.G(e.low)
+    integrator = tp1.Exercice()
+    trapeze , _ = integrator.ex_integration_numerique(e.integrand , e.low , e.high , N = 100 , M = 'trapeze')
+    variance = trapeze - exact_value ** 2
+    print(variance)
     # Import integrate function from tp1 and integrate h**2 * f
     # std = trapeze - I**2
     
