9e6cf9d9f4538dd17248b1954892e569fc155176
jose@ensi <jose@ensi>
2026-03-12 11:59:24 +0100

    add things for tp4 diffusion, missing graphs

--- tp4.py
+++ tp4.py
@@ -60,33 +60,104 @@ cos(theta) = 1 - 2FT = FT^(-1)
 
 """
 class Photon :
-    def __init__(self , z):
+    def __init__(self , z , costheta , phi):
         self.trajectory = []
         self.z = z
-        self.theta = None
-        self.phi = None
+        self.costheta = costheta
+        self.phi = phi
+        self.x , self.y = spherical_to_cartesian(z,costheta,phi)
+        self.current_diffusion = None
 
 def ZCDF(u):
     return 1 - np.exp(-alpha*u)
 
 def inverse_ZCDF(u):
-    return (-np.log(1 - ZCDF(u) ))/alpha
+    return (-np.log(1 - u ))/alpha
 
 def thetaCDF(u):
     return (1/2) * [1 - np.cos(u)]
 
 def inverse_costhetaCDF(u):
-    return 1 - 2*thetaCDF(u)
+    return 1 - 2*u
+
+def spherical_to_cartesian(r,costheta,phi):
+    theta = np.arccos(costheta)
+    sintheta = np.sin(theta)
+
+    x = r*sintheta*np.cos(phi)
+    y = r*sintheta*np.sin(phi)
+
+    return x,y
 
 
 
 def diffusion(n_photons):
-    z_uniform_samples = np.random.uniform(low = 0 , high=1 , size = n_photons)
+    photon_list = []
+    photons_left = n_photons
+    current_difusion = 0
+    in_photons_z = np.zeros(shape=(n_photons,))
 
-    print(z_uniform_samples)
+    while photons_left > 0 :
+        r_uniform_samples = np.random.uniform(low = 0 , high=1 , size = photons_left)
+        r_samples = inverse_ZCDF(r_uniform_samples)
 
-    z_samples = inverse_ZCDF(z_uniform_samples)
+        if current_difusion == 0:
+            costheta_samples = np.ones_like(r_samples)
+        else:
+            costheta_uniform_samples = np.random.uniform(low = 0 , high = 1 , size = photons_left )
+            costheta_samples = inverse_costhetaCDF(costheta_uniform_samples)
 
-    print(z_samples)
+        phi_samples = np.random.uniform(low=0 , high=2*np.pi , size = photons_left)
+        
 
-diffusion(10)
+        z_positions = np.multiply(r_samples,costheta_samples)
+
+        z_positions = z_positions + in_photons_z
+
+        mask_z_is_out = (z_positions < 0) | (z_positions > L)
+        if sum(mask_z_is_out) != 0:
+            out_photons = np.vstack([z_positions[mask_z_is_out] , costheta_samples[mask_z_is_out],
+                                    phi_samples[mask_z_is_out]]).T
+            
+            curr_list = list(map(lambda x: Photon(x[0] , x[1] , x[2]),out_photons))
+            for p in curr_list:
+                p.current_diffusion = current_difusion
+            photon_list.extend(curr_list)
+
+        in_photons_z = z_positions[~mask_z_is_out] 
+        photons_left = n_photons - len(photon_list)
+        current_difusion = current_difusion + 1
+
+
+    return photon_list
+
+        
+
+    
+
+
+
+diffused_photons = diffusion(10000)
+
+no_diffusion = []
+back_diffusion = []
+forward_diffusion = []
+
+for p in diffused_photons:
+    if p.current_diffusion == 0:
+        no_diffusion.append(p)
+    else:
+        if p.z <= 0 :
+            back_diffusion.append(p)
+        elif p.z >= L:
+            forward_diffusion.append(p)
+        else:
+            raise ValueError("forcas malignas")
+
+print(f"No diffusion : {len(no_diffusion)/len(diffused_photons):.2f}")
+print(f"Beer Lambert : {np.exp(-3):.2f}")
+print(f"Forward diffusion : {len(forward_diffusion)/len(diffused_photons):.2f}")
+print(f"Back diffusion : {len(back_diffusion)/len(diffused_photons):.2f}")
+
+
+
